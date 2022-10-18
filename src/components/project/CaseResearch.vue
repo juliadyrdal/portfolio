c91c31b2d68fe3d6a34a536ec39d5c6a81dbcd93
juliadyrdal <juliadyrdal@protonmail.com>
2022-10-18 19:33:05 +0200

    Add props to CaseResearch and replace lorem ipsum text.

--- src/components/project/CaseResearch.vue
+++ src/components/project/CaseResearch.vue
@@ -1,5 +1,15 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
+import { defineProps, ref } from "vue";
+
+const props = defineProps({
+  introduction: String,
+  goals: String,
+  methods: String,
+  questions: String,
+  atomic: String,
+  analysis: String,
+});
 </script>
 
 <template>
@@ -8,7 +18,7 @@ import { RouterLink, RouterView } from "vue-router";
         <div class="basis-1/2">
           <h3 class="pt-20 text-lg font-medium uppercase">Empathise</h3>
           <h2 class="py-6 text-4xl font-semibold leading-10 w-1/2">Research and discovery</h2>
-          <p class="pt-24 text-sm leading-6 w-9/12"><span class="font-semibold">Magna sunt aute nisi</span> amet veniam laboris. Dolor ut do consequat non aliqua id adipisicing pariatur in in eiusmod elit. Ullamco sint eiusmod anim veniam sint.</p>
+          <p class="pt-24 text-sm leading-6 w-9/12">{{ introduction }}</p>
         </div>
         <div class="basis-1/2 flex justify-center">
           <img src="../../assets/recycle.svg" alt="" class="w-44" />
@@ -17,47 +27,47 @@ import { RouterLink, RouterView } from "vue-router";
       <div class="ml-20 pb-4 overflow-x-scroll scrollbar">
         <ul class="my-12 grid grid-flow-col gap-8">
           <li class="">
-            <article class="w-72">
+            <article class="w-96">
               <div class="">
                 <img src="../../assets/challenge-1.png" alt="" class="" />
                 <h4 class="pt-8 text-base font-bold">Research goals</h4>
-                <p class="pt-3 text-sm"><span class="font-semibold">Magna sunt aute nisi</span> amet veniam laboris. Dolor ut do consequat non aliqua id adipisicing pariatur in in eiusmod elit. Ullamco sint eiusmod anim veniam sint.</p>
+                <p class="pt-3 text-sm">{{ goals }}</p>
               </div>
             </article>
           </li>
           <li class="">
-            <article class="w-72">
+            <article class="w-96">
               <div class="">
                 <img src="../../assets/challenge-1.png" alt="" class="" />
                 <h4 class="pt-8 text-base font-bold">Methods and techniques</h4>
-                <p class="pt-3 text-sm"><span class="font-semibold">Magna sunt aute nisi</span> amet veniam laboris. Dolor ut do consequat non aliqua id adipisicing pariatur in in eiusmod elit. Ullamco sint eiusmod anim veniam sint.</p>
+                <p class="pt-3 text-sm">{{ methods }}</p>
               </div>
             </article>
           </li>
-          <li class="">
-            <article class="w-72">
+          <!-- <li class="">
+            <article class="w-96">
               <div class="">
                 <img src="../../assets/challenge-1.png" alt="" class="" />
                 <h4 class="pt-8 text-base font-bold">Questions workshop</h4>
-                <p class="pt-3 text-sm"><span class="font-semibold">Magna sunt aute nisi</span> amet veniam laboris. Dolor ut do consequat non aliqua id adipisicing pariatur in in eiusmod elit. Ullamco sint eiusmod anim veniam sint.</p>
+                <p class="pt-3 text-sm">{{ questions }}</p>
               </div>
             </article>
-          </li>
+          </li> -->
           <li class="">
-            <article class="w-72">
+            <article class="w-96">
               <div class="">
                 <img src="../../assets/challenge-1.png" alt="" class="" />
                 <h4 class="pt-8 text-base font-bold">Atomic research</h4>
-                <p class="pt-3 text-sm"><span class="font-semibold">Magna sunt aute nisi</span> amet veniam laboris. Dolor ut do consequat non aliqua id adipisicing pariatur in in eiusmod elit. Ullamco sint eiusmod anim veniam sint.</p>
+                <p class="pt-3 text-sm">{{ atomic }}</p>
               </div>
             </article>
           </li>
           <li class="">
-            <article class="w-72">
+            <article class="w-96">
               <div class="">
                 <img src="../../assets/challenge-1.png" alt="" class="" />
                 <h4 class="pt-8 text-base font-bold">Analysis</h4>
-                <p class="pt-3 text-sm"><span class="font-semibold">Magna sunt aute nisi</span> amet veniam laboris. Dolor ut do consequat non aliqua id adipisicing pariatur in in eiusmod elit. Ullamco sint eiusmod anim veniam sint.</p>
+                <p class="pt-3 text-sm">{{ analysis }}</p>
               </div>
             </article>
           </li>
