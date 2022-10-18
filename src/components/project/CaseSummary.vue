d4706228327ea6e5354b6de6a3987ca90d5b25eb
juliadyrdal <juliadyrdal@protonmail.com>
2022-10-18 23:28:38 +0200

    Refactor: add named slots for paragraph columns to CaseSummary

--- src/components/project/CaseSummary.vue
+++ src/components/project/CaseSummary.vue
@@ -43,37 +43,9 @@ const props = defineProps({
       </div>
     </div>
     <div class="py-16 bg-surface-light">
-      <div class="container mx-auto grid grid-cols-2 gap-32 leading-7">
-        <div>
-          <p class="pb-6">
-            This UX project was a course assignment lasting four weeks, for the UXD programme at Noroff. The brief was open ended, to prototype a (mid-fidelity) solution to encourage its users to conserve water. The greater context was the global fresh water crisis and drinking water shortages in severely affected areas. As water consumption in Norway does not directly affect these global issues, I really wanted to find a way to connect local efforts with global solutions. In order to accomplish this, I did an extensive literary review of sources related to freshwater hydrology, draught causes, as well as the ecosystem of water supply, consumption and waste in Norway.
-          </p>
-          <p class="pb-6">
-            In order to be able to affect behavioural change, I needed to understand the target group's awareness of and attitudes towards water consumption and conservation on an individual, local and global level. This objective guided my primary research, in the form of interviews, leading to the insights that the target group had very low awareness of the consequences of their direct consumption of water, a strong belief that over consumption in Norway didn't matter, and as good as no awareness of how their actions could affect water shortages outside of Norway.
-          </p>
-          <p>
-            From my secondary research I knew that when Norwegians install a (local government approved) water meter in their homes, and thereby switch to a volumetric pricing model, their consumption levels plummet. I also found a pilot project by Bodø municipality, where they tested the use of digital water meters, where the inhabitants had access to their own real-time consumption data, and the ability to share this data with third parties through an API (application programming interface).
-          </p>
-        </div>
-        <div>
-          <p class="pb-6">
-            As digital water meters have been introduced in several smaller municipalities, where the barrier is lower, I have made the assumption that this upgrade will develop similarily to the mandatory switch to digital electricity meters a few years ago, which now seamlessly supports a number of interfaces for understanding and controlling our home's electricity consumption.
-          </p>
-          <p>
-           On the basis of my secondary research regarding the causes of global shortages, the consequences of local consumption, the target users' behaviour, attitudes and awareness, as well as my assumptions around the adoption of digital water meters, I ideated a solution that would give its users real-time feed back of their water consumption, including costs and enable them to join challenges to reduce their consumption, as well as provide relevant advice for how to do so. In In order to increase awareness of their indirect consumption of water in draught affected areas, users who complete challenges are awarded badges based on volume saved, which is then used to illustrate how much water they consume indirectly from draught affected areas, during common activities, and suggest ways to mitigate it. 
-          </p>
-          <div class="grid grid-cols-3 xl:grid-cols-11 mt-12">
-            <a
-              target="_blank"
-              :href="prototypeLink"
-              class="col-start-2 col-span-2 xl:col-start-6 xl:col-span-6 px-6 py-4 bg-white text-sm hover:underline"
-              >View Figma prototype<img
-                src="../../assets/icons/button-arrow.svg"
-                alt="arrow icon"
-                class="inline pl-4"
-            /></a>
-          </div>
-        </div>
+      <div class="container mx-auto grid grid-cols-2 gap-20 leading-7">
+        <slot name="columnOne"></slot>
+        <slot name="columnTwo"></slot>
       </div>
     </div>
   </section>
--- src/views/DropsView.vue
+++ src/views/DropsView.vue
@@ -18,10 +18,45 @@ import imageUrl from '@/assets/drops-thumbnail.png'
     <CaseIntroduction titleHeading="Global Issues" heading="Local solutions" subHeading="A user experience design course assignment" paragraphOne="" paragraphTwo="Because of the visible abundance of water, Norwegians don’t consider their own consumption of water - it’s always available, on demand, like breathing air.
 In addition to being so ubiquitous, water is very cheap and the overwhelming majority pay a flat rate, no matter how much water they use.
 Because Norwegians think about water as an inexhaustable, renewable resource, they are not aware of the environmental cost and carbon footprint of their own direct water consumption. These factors might be contributing to the fact that Norwegians on average consume 180 litres of tap water each day, a record in the Nordics, and twice as much as our Danish neighbours." />
-    <CaseSummary projectType="course assignment" duration="4 weeks, spring 2022" role="solo UX project" prototypeLink="https://www.figma.com/proto/osSqZdc1q4TgZ1v32x3RPf/Wireframes?page-id=33%3A92&node-id=33%3A97&scaling=scale-down&starting-point-node-id=33%3A93&show-proto-sidebar=1" />
-    <CaseChallenges />
+    <CaseSummary projectType="course assignment" duration="4 weeks, spring 2022" role="solo UX project">
+      <template #columnOne>
+        <div>
+          <p class="pb-6">
+            The assignment brief was open ended, to prototype a (mid-fidelity) solution to encourage its users to conserve water. The greater context was the global fresh water crisis and drinking water shortages in severely affected areas. As water consumption in Norway does not directly affect these global issues, I really wanted to find a way to connect local efforts with global solutions. In order to accomplish this, I did an extensive literary review of sources related to freshwater hydrology, draught causes, as well as the ecosystem of water supply, consumption and waste in Norway.
+          </p>
+          <p class="pb-6">
+            <span class="font-semibold">In order to be able to affect behavioural change</span>, I needed to understand the target group's awareness of and attitudes towards water consumption and conservation on an individual, local and global level. This objective guided my primary research, in the form of interviews, leading to the insights that the target group had very low awareness of the consequences of their direct consumption of water, a strong belief that over consumption in Norway didn't matter, and as good as no awareness of how their actions could affect water shortages outside of Norway.
+          </p>
+          <p>
+            <span class="font-semibold">From my secondary research I knew</span> that when Norwegians install a (local government approved) water meter in their homes, and thereby switch to a volumetric pricing model, their consumption levels plummet. I also found a pilot project by Bodø municipality, where they tested the use of digital water meters, where the inhabitants had access to their own real-time consumption data, and the ability to share this data with third parties through an API
+          </p>
+        </div>
+      </template>
+      <template #columnTwo>
+        <div>
+          <p class="pb-6">
+            (application programming interface). As digital water meters have been introduced in several smaller municipalities, where the barrier is lower, I have made the assumption that this upgrade will develop similarily to the mandatory switch to digital electricity meters a few years ago, which now seamlessly supports a number of interfaces for understanding and controlling our home's electricity consumption.
+          </p>
+          <p>
+           On the basis of my secondary research regarding the causes of global shortages, the consequences of local consumption, the target users' behaviour, attitudes and awareness, as well as my assumptions around the adoption of digital water meters, I ideated a solution that would give its users real-time feed back of their water consumption, including costs and enable them to join challenges to reduce their consumption, as well as provide relevant advice for how to do so. In In order to increase awareness of their indirect consumption of water in draught affected areas, users who complete challenges are awarded badges based on volume saved, which is then used to illustrate how much water they consume indirectly from draught affected areas, during common activities, and suggest ways to mitigate it. 
+          </p>
+          <div class="grid grid-cols-3 xl:grid-cols-11 mt-12">
+            <a
+              target="_blank"
+              href="https://www.figma.com/proto/osSqZdc1q4TgZ1v32x3RPf/Wireframes?page-id=33%3A92&node-id=33%3A97&scaling=scale-down&starting-point-node-id=33%3A93&show-proto-sidebar=1"
+              class="col-start-2 col-span-2 xl:col-start-6 xl:col-span-6 px-6 py-4 bg-white text-sm hover:underline"
+              >View Figma prototype<img
+                src="../assets/icons/button-arrow.svg"
+                alt="arrow icon"
+                class="inline pl-4"
+            /></a>
+          </div>
+        </div>
+      </template>
+    </CaseSummary>
     <CaseProcess id="process" />
     <CaseResearch introduction="Magna sunt aute nisi amet veniam laboris. Dolor ut do consequat non aliqua id adipisicing pariatur in in eiusmod elit. Ullamco sint eiusmod anim veniam sint." goals="To discover the target users’ awareness, attitudes and behaviour related to freshwater conservation and consumption on an individual, local and global scale." methods="I used the secondary research method literature review, to get more domain knowledge and to understand the technical challenges. As a primary research methods I interviewed potential users in the target group." questions="" atomic="I have used the atomic research model to organise research data, including facts, themes and insights. This will be useful for Fjordtours in their continued research, to strengthen insights, use established facts in new insights, and to share knowledge between departments." analysis="To analyse the research data from both primary and secondary research, I used affinity mapping to sort facts and discover patterns and themes. " />
     <ResearchFindingsTabs />
+    <CaseChallenges />
   </main>
 </template>
