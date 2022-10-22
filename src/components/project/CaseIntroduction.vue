68f39aca33905c42d2618026a84cb7e70e7c4520
juliadyrdal <juliadyrdal@protonmail.com>
2022-10-22 18:28:33 +0200

    Adjust alignment on CaseIntroduction component

--- src/components/project/CaseIntroduction.vue
+++ src/components/project/CaseIntroduction.vue
@@ -6,8 +6,7 @@ const props = defineProps({
 	titleHeading: String,
 	heading: String,
 	subHeading: String,
-	paragraphOne: String,
-	paragraphTwo: String
+	paragraph: String,
 })
 </script>
 
@@ -18,9 +17,8 @@ const props = defineProps({
       <p class="py-3 text-sm text-font-primary font-medium uppercase text-font-primary">{{ subHeading }}</p>
       <article class="mt-16 flex items-center bg-surface-primary items-stretch">
         <img src="../../assets/drops-thumbnail.png" alt="" class="h-full object-cover">
-        <div class="mx-16 pr-12">
-          <p class="py-4 leading-7">{{ paragraphOne }}</p>
-          <p class="py-4 leading-7">{{ paragraphTwo }}</p>
+        <div class="mx-16 pr-12 grid grid-cols-1 gap-6">
+          <div class="flex flex-col justify-center"><p class="leading-7">{{ paragraph }}</p></div>
         </div>
       </article>
     </section>
