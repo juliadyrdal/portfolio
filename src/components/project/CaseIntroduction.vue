6d97deac5be8f77fe61ab962661fb70f3dedbd99
juliadyrdal <juliadyrdal@protonmail.com>
2022-10-23 22:11:51 +0200

    Add external urls to links on ProjectsView and HomeView

--- src/components/project/CaseIntroduction.vue
+++ src/components/project/CaseIntroduction.vue
@@ -3,23 +3,33 @@ import { RouterLink, RouterView } from "vue-router";
 import { defineProps, ref } from "vue";
 
 const props = defineProps({
-	titleHeading: String,
-	heading: String,
-	subHeading: String,
-	paragraph: String,
-})
+  titleHeading: String,
+  heading: String,
+  subHeading: String,
+  paragraph: String,
+});
 </script>
 
 <template>
-	<section class="container mx-auto my-32">
-      <p class="text-lg text-font-primary font-medium">{{ titleHeading }}</p>
-      <h2 class="pt-4 pb-4 text-black text-6xl font-medium">{{ heading }}</h2>
-      <p class="py-3 text-sm text-font-primary font-medium uppercase text-font-primary">{{ subHeading }}</p>
-      <article class="mt-16 flex items-center bg-surface-primary items-stretch">
-        <img src="../../assets/drops-thumbnail.png" alt="" class="h-full object-cover">
-        <div class="mx-16 pr-12 grid grid-cols-1 gap-6">
-          <div class="flex flex-col justify-center"><p class="leading-7">{{ paragraph }}</p></div>
+  <section class="container mx-auto my-32">
+    <p class="text-lg text-font-primary font-medium">{{ titleHeading }}</p>
+    <h2 class="pt-4 pb-4 text-black text-6xl font-medium">{{ heading }}</h2>
+    <p
+      class="py-3 text-sm text-font-primary font-medium uppercase text-font-primary"
+    >
+      {{ subHeading }}
+    </p>
+    <article class="mt-16 flex items-center bg-surface-primary items-stretch">
+      <img
+        src="../../assets/drops-thumbnail.png"
+        alt=""
+        class="h-full object-cover"
+      />
+      <div class="mx-16 pr-12 grid grid-cols-1 gap-6">
+        <div class="flex flex-col justify-center">
+          <p class="leading-7">{{ paragraph }}</p>
         </div>
-      </article>
-    </section>
+      </div>
+    </article>
+  </section>
 </template>
--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -7,7 +7,7 @@ import { RouterLink, RouterView } from "vue-router";
   <section class="container mx-auto mt-20 font-sans text-black">
     <h1 class="py-4 text-8xl font-medium">Projects</h1>
     <!-- <p class="py-8 w-2/3 leading-8">Magna sunt aute nisi amet veniam laboris. Dolor ut do consequat non aliqua id adipisicing pariatur in in eiusmod elit. Ullamco sint eiusmod anim veniam sint. Culpa elit ullamco ullamco esse minim ut dolor.</p> -->
-    <p class="py-2 pt-8 font-semibold"><a target="_blank" href="https://github.com/juliadyrdal" class="pr-2 hover:underline">Github</a> | <a href="#" class="pl-2 hover:underline">LinkedIn</a></p>
+    <p class="py-2 pt-8 font-semibold"><a target="_blank" href="https://github.com/juliadyrdal" class="pr-2 hover:underline">Github</a> | <a target="_blank" href="https://www.linkedin.com/in/juliadyrdal" class="pl-2 hover:underline">LinkedIn</a></p>
   </section>
   <section class="container mx-auto mt-12">
     <article class="flex items-center bg-surface-secondary">
